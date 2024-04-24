371f0f7ebd0556bd6b43edb24b1e1921896bd020
Joel Van Eenwyk <joel.vaneenwyk@gmail.com>
2024-04-24 14:10:37 -0700

    Cleanup run tests
    Signed-off-by: Joel Van Eenwyk <joel.vaneenwyk@gmail.com>

--- .github/scripts/run_tests.sh
+++ .github/scripts/run_tests.sh
@@ -2,4 +2,10 @@
 
 set -eax
 
-coverage run -m pytest pywinauto/unittests/test_atspi_element_info.py pywinauto/unittests/test_atspi_wrapper.py pywinauto/unittests/test_atspi_controls.py pywinauto/unittests/test_clipboard_linux.py pywinauto/unittests/test_keyboard.py pywinauto/unittests/test_application_linux.py
+coverage run -m pytest \
+    pywinauto/unittests/test_atspi_element_info.py \
+    pywinauto/unittests/test_atspi_wrapper.py \
+    pywinauto/unittests/test_atspi_controls.py \
+    pywinauto/unittests/test_clipboard_linux.py \
+    pywinauto/unittests/test_keyboard.py \
+    pywinauto/unittests/test_application_linux.py
