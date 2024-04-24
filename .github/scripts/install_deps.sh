ae01991ee3c247b88963a78b908b587e7c949f93
Joel Van Eenwyk <joel.vaneenwyk@gmail.com>
2024-04-24 13:36:11 -0700

    Fixup dependency install
    Signed-off-by: Joel Van Eenwyk <joel.vaneenwyk@gmail.com>

--- .github/scripts/install_deps.sh
+++ .github/scripts/install_deps.sh
@@ -1,25 +1,43 @@
 #!/usr/bin/sh
 
-python -m pip install --upgrade pip
-sudo apt update -y
-sudo apt upgrade
-sudo apt install -y --no-install-recommends python-setuptools tk-dev python-tk
-sudo apt install -y --no-install-recommends python3 python3-pip python3-setuptools
-sudo apt install -y --no-install-recommends python-xlib
-sudo apt install -y --no-install-recommends xsel
-sudo apt install -y libjavascriptcoregtk-4.0-18
-pip install coverage codecov
-pip install python-xlib --upgrade
-pip install mock --upgrade
-python -m pip install pytest
-sudo apt install -y build-essential libssl-dev
-sudo apt install -y qt5-default
-sudo apt install -y libxkbcommon-x11-0
-sudo apt install -y python3-gi gobject-introspection gir1.2-gtk-3.0
-sudo apt install -y at-spi2-core
-sudo apt install -y python3-pyatspi
-sudo apt install -y xvfb x11-utils libxkbcommon-x11-0 \
-    libxcb-icccm4 libxcb-image0 libxcb-keysyms1 libxcb-randr0 \
-    libxcb-render-util0 libxcb-xinerama0 libxcb-xfixes0 xdotool
-#pip3 install scikit-build # required for cmake>=3.23.0
-pip3 install cmake==3.22.6
+set -eax
+
+_install() {
+    sudo apt-get update -y && sudo apt-get upgrade -y
+
+    sudo apt-get install -y --no-install-recommends \
+        python-setuptools tk-dev python-tk \
+        python3 python3-pip python3-setuptools
+
+    sudo apt-get install -y --no-install-recommends \
+        xsel \
+        libjavascriptcoregtk-4.0-18 \
+        build-essential libssl-dev mesa-common-dev \
+        libxkbcommon-x11-0 \
+        python3-gi gobject-introspection gir1.2-gtk-3.0 \
+        at-spi2-core \
+        xvfb x11-utils libxkbcommon-x11-0 \
+        libxcb-icccm4 libxcb-image0 libxcb-keysyms1 libxcb-randr0 \
+        libxcb-render-util0 libxcb-xinerama0 libxcb-xfixes0 xdotool
+
+    # 'qt5-default' is deprecated and no longer available in Ubuntu 21.04+
+    sudo apt-get install -y --no-install-recommends \
+        qtbase5-dev qtchooser qt5-qmake qtbase5-dev-tools
+
+    sudo apt-get install -y --no-install-recommends python3-pyatspi
+    sudo apt-get update -y && sudo apt-get upgrade -y
+}
+
+_install_python_packages() {
+    python3 -m pip install --upgrade \
+        pip python-xlib mock
+
+    # 'scikit-build' is required for cmake>=3.23.0
+    python3 -m pip install \
+        coverage codecov \
+        pytest \
+        scikit-build cmake==3.22.6
+}
+
+_install
+_install_python_packages
