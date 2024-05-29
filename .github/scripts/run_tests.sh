f87a4c55fc9384870cde2427f7df0afa7747125f
Joel Van Eenwyk <joel.vaneenwyk@gmail.com>
2024-05-29 15:01:12 -0700

    Fix paths to unit tests

--- .github/scripts/run_tests.sh
+++ .github/scripts/run_tests.sh
@@ -3,9 +3,9 @@
 set -eax
 
 coverage run -m pytest \
-    pywinauto/unittests/test_atspi_element_info.py \
-    pywinauto/unittests/test_atspi_wrapper.py \
-    pywinauto/unittests/test_atspi_controls.py \
-    pywinauto/unittests/test_clipboard_linux.py \
-    pywinauto/unittests/test_keyboard.py \
-    pywinauto/unittests/test_application_linux.py
+    src/pywinauto/unittests/test_atspi_element_info.py \
+    src/pywinauto/unittests/test_atspi_wrapper.py \
+    src/pywinauto/unittests/test_atspi_controls.py \
+    src/pywinauto/unittests/test_clipboard_linux.py \
+    src/pywinauto/unittests/test_keyboard.py \
+    src/pywinauto/unittests/test_application_linux.py
