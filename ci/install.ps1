39d89c31142638fe7e36878a26bd8ec46bbf806c
Joel Van Eenwyk <joel.vaneenwyk@gmail.com>
2024-05-29 20:09:15 -0700

    wip: Fix PowerShell scripts

--- ci/install.ps1
+++ ci/install.ps1
@@ -1,18 +1,19 @@
 param(
-    [string]$PythonPath = "C:\Python34-x64",
-    [string]$PythonVersion = "3.4",
+    [string]$PythonPath = 'C:\Python311-x64',
+    [string]$PythonVersion = '3.11',
     [int]$PythonArch = 64,
-    [string]$UIASupport = "NO"
+    [string]$UIASupport = 'NO'
 )
 
-$MinicondaUrl = "http://repo.continuum.io/miniconda/"
+$MinicondaUrl = 'http://repo.continuum.io/miniconda/'
 
 function DownloadMiniconda($pythonVersion, $platformSuffix) {
     $webClient = New-Object System.Net.WebClient
-    $filename = if ($pythonVersion -match "3.4") { "Miniconda3-3.8.3-Windows-$platformSuffix.exe" } else { "Miniconda-3.8.3-Windows-$platformSuffix.exe" }
+    $url = 'https://repo.anaconda.com/miniconda/Miniconda3-py311_24.4.0-0-Windows-x86_64.exe'
+    $filename = if ($pythonVersion -match '3.11') { "Miniconda3-py311_24.4.0.0-Windows-$platformSuffix.exe" } else { "Miniconda-py311_24.4.0.0-$platformSuffix.exe" }
     $url = "$MinicondaUrl$filename"
 
-    $basePath = Join-Path $pwd.Path ""
+    $basePath = Join-Path $pwd.Path ''
     $filePath = Join-Path $basePath $filename
 
     if (Test-Path $filePath) {
@@ -27,13 +28,15 @@ function DownloadMiniconda($pythonVersion, $platformSuffix) {
         try {
             $webClient.DownloadFile($url, $filePath)
             break
-        } Catch [Exception] {
+        }
+        Catch [Exception] {
             Start-Sleep 1
         }
     }
     if (Test-Path $filePath) {
         Write-Host "File saved at $filePath"
-    } else {
+    }
+    else {
         # Retry once to get the error message if any at the last try
         $webClient.DownloadFile($url, $filePath)
     }
@@ -46,16 +49,17 @@ function InstallMiniconda($pythonVersion, $architecture, $pythonHome) {
         Write-Host "$pythonHome already exists, skipping."
         return $false
     }
-    $platformSuffix = if ($architecture -eq 32) { "x86" } else { "x86_64" }
+    $platformSuffix = if ($architecture -eq 32) { 'x86' } else { 'x86_64' }
     $filePath = DownloadMiniconda $pythonVersion $platformSuffix
     Write-Host "Installing $filePath to $pythonHome"
-    $installLog = Join-Path $pythonHome ".log"
-    $args = "/S /D=$pythonHome"
-    Write-Host "$filePath $args"
-    Start-Process -FilePath $filePath -ArgumentList $args -Wait -Passthru
+    $installLog = Join-Path $pythonHome '.log'
+    $arguments = "/S /D=$pythonHome"
+    Write-Host "$filePath $arguments"
+    Start-Process -FilePath $filePath -ArgumentList $arguments -Wait -PassThru
     if (Test-Path $pythonHome) {
         Write-Host "Python $pythonVersion ($architecture-bit) installation complete"
-    } else {
+    }
+    else {
         Write-Host "Failed to install Python in $pythonHome"
         Get-Content -Path $installLog
         Exit 1
@@ -63,31 +67,58 @@ function InstallMiniconda($pythonVersion, $architecture, $pythonHome) {
 }
 
 function InstallCondaPackages($pythonHome, $spec) {
-    $condaPath = Join-Path $pythonHome "Scripts\conda.exe"
-    $args = "install --yes $spec"
-    Write-Host ("conda " + $args)
-    Start-Process -FilePath "$condaPath" -ArgumentList $args -Wait -Passthru
+    $condaPath = Join-Path $pythonHome 'Scripts\conda.exe'
+    $arguments = "install --yes $spec"
+    Write-Host ('conda ' + $arguments)
+    Start-Process -FilePath "$condaPath" -ArgumentList $arguments -Wait -PassThru
 }
 
 function UpdateConda($pythonHome) {
-    $condaPath = Join-Path $pythonHome "Scripts\conda.exe"
-    Write-Host "Updating conda..."
-    $args = "update --yes conda"
-    Write-Host "$condaPath $args"
-    Start-Process -FilePath "$condaPath" -ArgumentList $args -Wait -Passthru
+    $condaPath = Join-Path $pythonHome 'Scripts\conda.exe'
+    Write-Host 'Updating conda...'
+    $arguments = 'update --yes conda'
+    Write-Host "$condaPath $arguments"
+    Start-Process -FilePath "$condaPath" -ArgumentList $arguments -Wait -PassThru
 }
 
 function InstallComtypes($pythonHome) {
-    $pipPath = Join-Path $pythonHome "Scripts\pip.exe"
-    $args = "install comtypes"
-    Start-Process -FilePath "$pipPath" -ArgumentList $args -Wait -Passthru
+    $pipPath = Join-Path $pythonHome 'Scripts\pip.exe'
+    $arguments = 'install comtypes'
+    Start-Process -FilePath "$pipPath" -ArgumentList $arguments -Wait -PassThru
+}
+
+function UpdateEnvironmentVairbale() {
+    [CmdletBinding(PositionalBinding = $false)]
+    Param(
+        [Parameter(Mandatory = $true, Position = 0)]
+        [string]
+        $Name,
+
+        [Parameter(Mandatory = $true, Position = 1)]
+        [AllowEmptyString()]
+        [string]
+        $Value,
+
+        [Parameter(Mandatory = $true, Position = 2)]
+        [System.EnvironmentVariableTarget]
+        $Target
+    )
+
+    $Old = [System.Environment]::GetEnvironmentVariable($Name, $Target)
+    if ("$Old" -Ne "$Value") {
+        Write-Host "Update: $Name ($Target)"
+        Write-Host "    Old: $Old"
+        Write-Host "    New: $Value"
+        [System.Environment]::SetEnvironmentVariable($Name, $Value, $Target)
+    }
 }
 
 function main() {
     try {
         $currentResolution = Get-DisplayResolution
         Write-Host "Current resolution: $currentResolution"
-    } Catch [Exception] {
+    }
+    Catch [Exception] {
         Write-Host "Can't print current resolution. Get-DisplayResolution cmd is not available"
     }
 
@@ -95,9 +126,33 @@ function main() {
     Write-Host "PYTHON_VERSION=$PythonVersion"
     Write-Host "PYTHON_ARCH=$PythonArch"
 
-    if ($UIASupport -eq "YES") {
-        InstallComtypes $PythonPath
+    try {
+        $InstallerExe = 'rye-x86_64-windows.exe'
+        $InstallerUrl = 'https://github.com/astral-sh/rye/releases/latest/download/$filePath'
+
+        Write-Host 'Downloading Rye installer'
+        Invoke-WebRequest -UseBasicParsing -o $InstallerExe $InstallerUrl
+        Write-Host 'Executing: rye self install'
+        & $InstallerExe self install --yes
+        Write-Host 'Executing: rye self update'
+        & $InstallerExe self update
+
+        if (Test-Path $pythonHome) {
+            Write-Host "Python $pythonVersion ($architecture-bit) installation complete"
+        }
     }
+    catch {
+        <#Do this if a terminating exception happens#>
+        # cargo install --git https://github.com/astral-sh/rye rye
+    }
+    finally {
+        <#Do this after the try block regardless of whether an exception occurred or not#>
+    }
+
+    if ($UIASupport -eq 'YES') {
+        # InstallComtypes $PythonPath
+    }
+
     # InstallMiniconda $PythonVersion $PythonArch $PythonPath
     # UpdateConda $PythonPath
     # InstallCondaPackages $PythonPath "pywin32 Pillow coverage nose"
